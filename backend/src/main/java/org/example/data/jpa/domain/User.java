71f113a773facd5ecea760b9d1918d3b5b26a7c4
DobbsZerker <nickdobbin@hotmail.com>
2022-08-08 12:34:13 -0230

    Started backend

---
+++ backend/src/main/java/org/example/data/jpa/domain/User.java
@@ -0,0 +1,54 @@
+package org.example.data.jpa.domain;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String userName;
+    private String password;
+    private boolean authorized;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    public void setUsername(String username) {
+        this.userName = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAuthorized() {
+        return authorized;
+    }
+
+    public void setAuthorized(boolean authorized) {
+        this.authorized = authorized;
+    }
+}
+
