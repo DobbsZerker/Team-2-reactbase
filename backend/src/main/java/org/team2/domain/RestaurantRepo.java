e6af088093b4e04a61cdb4615640bba5d82811b4
dunnjon <95690225+dunnjon@users.noreply.github.com>
2022-08-09 19:53:42 -0230

    added search page

--- backend/src/main/java/org/team2/domain/RestaurantRepo.java
+++ backend/src/main/java/org/team2/domain/RestaurantRepo.java
@@ -3,7 +3,8 @@ package org.team2.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
     @RepositoryRestResource(collectionResourceRel = "restaurant", path = "restaurant")
