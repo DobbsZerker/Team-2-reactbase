e89e425a4ce6fc572664ab0940751855e2f55820
DobbsZerker <nickdobbin@hotmail.com>
2022-08-10 18:06:49 -0230

    Changed restaurant apllication and some file changes in restaurant

--- backend/src/main/java/org/team2/RestaurantApplication.java
+++ backend/src/main/java/org/team2/RestaurantApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerApiApplication {
+public class RestaurantApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApplication.class, args);
