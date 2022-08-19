ace93ddb575d5c60c66c590b355f2aa33dc3bce0
DobbsZerker <nickdobbin@hotmail.com>
2022-08-19 13:29:10 -0230

    Changed Header and Footer to reflect more properly

--- backend/React_Base/src/components/Footer.js
+++ backend/React_Base/src/components/Footer.js
@@ -6,7 +6,9 @@ const Footer = () => {
     return (
         <footer>
             <hr/><hr/>
-            <p>Coded by: Stephen Squire</p>
+            <p>Coded by:  Front end:Stephen Squire , refactored by Nicholas, Johnathon and Shane </p>
+            <p>Backend coded: by Nicholas, Johnathon and Shane</p>
+
             <p> &copy; Copyright 2022, All Rights Reserved </p>
             <p>Credit to Yelp for the sample dataset used for testing</p>
             <p>Built using create-react-app</p>
